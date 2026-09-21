c5badc34ece043733c2d111798ca5ec4aa243add
rriver06 <mauricioriveroruiz24@gmail.com>
2026-09-21 02:40:02 -0400

    feat: added flake dir for easier nh use

--- home/laptop.nix
+++ home/laptop.nix
@@ -11,12 +11,18 @@
     ./apps/delta.nix
   ];
 
-  # Make sure to check the username
-  home.username = "rriver06";
-  home.homeDirectory = "/home/rriver06";
-  
-  # Make sure this version is the same as the one in the main configuration.nix
-  home.stateVersion = "26.05"; 
+  home = {
+    # Make sure to check the username
+    username = "rriver06";
+    homeDirectory = "/home/rriver06";
+
+    sessionVariables = {
+      NHFLAKE = "${config.home.homeDirectory}/.config/nixos";
+    };
+
+    # Make sure this version is the same as the one in the main configuration.nix
+    stateVersion = "26.05";
+  };
 
   programs.home-manager.enable = true;
 }
